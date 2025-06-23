451b574806e45a0d4139d8ee6c430059c72f6c01
Eddy Mead <wilddamon@gmail.com>
2025-06-23 15:41:52 +1000

    Add a run.sh file

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo_then_run() {
+    echo "\n\n$1"
+    $1
+}
+
+echo_then_run "python basic-processing/basic_processing.py"
+echo_then_run "python basic-processing/merge_datasets.py"
+
+# echo_then_run "python gemini/extract_gemini.py"
+echo_then_run "python gemini/merge_gemini.py"
+echo_then_run "python gemini/add_country_income_status.py"
+
+echo_then_run "python ranking/rank_relevance.py"
+
