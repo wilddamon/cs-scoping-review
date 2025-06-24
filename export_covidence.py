b95b6ad1812eecc25c2a0dc366315b76d8887b9a
Eddy Mead <wilddamon@gmail.com>
2025-06-24 16:42:33 +1000

    Add export to endnote/covidence script

---
+++ export_covidence.py
@@ -0,0 +1,37 @@
+import pandas
+
+
+data = pandas.read_csv("outputs/ranked_abstracts-2025_02_25T04_30.csv")
+
+
+def export_endnote_txt(group, name):
+    g = group[["title", "abstract", "year", "authors", "journal", "doi"]].copy()
+    g["title"] = g["title"].apply(lambda t: t.title())
+    g["abstract"] = g["abstract"].apply(lambda a: a.replace('"', ""))
+    g["year"] = g["year"].apply(lambda y: f"{y:.0f}")
+    g = g.rename(
+        columns={
+            "title": "Title",
+            "abstract": "Abstract",
+            "year": "Year",
+            "authors": "Author",
+            "journal": "Journal",
+            "doi": "DOI",
+        }
+    )
+    # path_or_buf=None -> return as string.
+    # sep="\t" -> tab delimit
+    # index=False -> do not output the index column
+    s = g.to_csv(path_or_buf=None, sep="\t", index=False)
+    s = "*Journal Article\n" + s
+    with open(f"outputs/for_endnote_import/{name}.txt", "w") as f:
+        f.write(s)
+
+
+group = data[data["relevance"] > 0]
+export_endnote_txt(group, f"included")
+
+
+data[(data["manual_assessment"] == "YES") & (data["relevance"] <= 0)].to_csv(
+    "outputs/false_negative.csv"
+)
