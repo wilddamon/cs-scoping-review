0e953999089bcb9aff3abb848e7236df8b3e0b89
Eddy Mead <wilddamon@gmail.com>
2026-03-19 13:09:46 +1100

    Improve run.sh to use -x option, fix modified filenames

--- run.sh
+++ run.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-echo_then_run() {
-    echo "\n\n$1"
-    $1
-}
+set -x
 
-echo_then_run "python database-search-results/parse_cinahl_psycinfo_set.py"
-echo_then_run "python database-search-results/parse_ovid_medline_embase_set.py"
-echo_then_run "python database-search-results/parse_pubmed_set.py"
-echo_then_run "python database-search-results/parse_scopus_set.py"
+python database-search-results/parse_cinahl_set.py
+python database-search-results/parse_ovid_sets.py
+python database-search-results/parse_pubmed_set.py
+python database-search-results/parse_scopus_set.py
 
-echo_then_run "python basic-processing/basic_processing.py"
-echo_then_run "python basic-processing/merge_datasets.py"
+python basic-processing/basic_processing.py
+python basic-processing/merge_datasets.py
 
-echo_then_run "python gemini/extract_gemini.py"
-echo_then_run "python gemini/validate_responses.py"
-echo_then_run "python gemini/merge_gemini.py"
-echo_then_run "python gemini/add_country_income_status.py"
+python gemini/extract_gemini.py
+python gemini/validate_responses.py
+python gemini/merge_gemini.py
+python gemini/add_country_income_status.py
 
-echo_then_run "python ranking/rank_relevance.py"
-echo_then_run "python export_endnote.py"
+python ranking/rank_relevance.py
+python export_endnote.py
