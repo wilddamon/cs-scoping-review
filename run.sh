c5c0687193a44a62a75cdc4fa0c4e633682d9018
Eddy Mead <wilddamon@gmail.com>
2026-03-19 16:48:18 +1100

    Make sure directory structure is in place for first run in run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,15 @@
 
 set -x -e
 
+if [ ! -d outputs/database-search-results ]; then
+    mkdir outputs/database-search-results
+    mkdir outputs/basic-processing
+    mkdir outputs/basic-processing/basic-exclusions
+    mkdir outputs/basic-processing/dupes-removed
+    mkdir outputs/for_covidence_import
+    mkdir outputs/for_endnote_import
+fi
+
 python database-search-results/parse_cinahl_set.py
 python database-search-results/parse_ovid_sets.py
 python database-search-results/parse_pubmed_set.py
